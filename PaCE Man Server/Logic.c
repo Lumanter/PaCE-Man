0ff9587efc7ec7016923e037c7873bb6e1e60d71
Álvaro Vargas Molina <36602950+AlvaroVargasM@users.noreply.github.com>
2020-08-03 03:00:59 -0700

    Added the linked list struct for managing the pills in the Game struct

--- PaCE Man Server/Logic.c
+++ PaCE Man Server/Logic.c
@@ -4,6 +4,8 @@
 
 #include "Logic.h"
 
+// Pair - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 Pair create_pair(int x,int y){
     Pair p;
     p.x = x;
@@ -11,6 +13,7 @@ Pair create_pair(int x,int y){
     return p;
 }
 
+// Pac-Man - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 Pac_Man create_pac_man(){
     Pac_Man p_m;
     p_m.pos = create_pair(200,300); // Starting pos
@@ -27,6 +30,7 @@ void modify_pac_man_position(Pac_Man* p_m_ptr,Pair new_pos){
     p_m_ptr->pos = new_pos;
 }
 
+// Ghost - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 Ghost create_ghost(int color) {
     Ghost g;
     g.pos = create_pair(200,180); // Starting pos
@@ -47,18 +51,46 @@ void deactivate_ghost(Ghost* g){
     g->active = false;
 }
 
+// Pill - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 Pill create_pill(Pair pos){
     Pill p;
     p.pos = pos;
     return p;
 }
 
+P_Node create_p_node(){
+    P_Node temp;
+    temp = (P_Node)malloc(sizeof(struct Linked_List_Pill));
+    temp->next = NULL;
+    return temp;
+}
+
+P_Node add_p_node(P_Node head,Pill pill){
+    P_Node temp, p;
+    temp = create_p_node();
+    temp->pill = pill;
+
+    if (head == NULL){
+        head = temp;
+    }
+    else {
+        p = head;
+        while (p->next != NULL){
+            p = p->next;
+        }
+        p->next = temp;
+    }
+    return head;
+}
+
+// Game - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 Game create_game(){
     Game game;
     game.level = 1;
     game.lives = 3;
     game.pac_man = create_pac_man();
     game.pill_active = false;
+    game.pills = NULL;
     game.ghosts[0] = create_ghost(1);
     game.ghosts[1] = create_ghost(2);
     game.ghosts[2] = create_ghost(3);
@@ -79,4 +111,8 @@ void update_game_ghosts_pos(Game *game,Pair* new_pos){
     modify_ghost_position(&game->ghosts[1],new_pos[1]);
     modify_ghost_position(&game->ghosts[2],new_pos[2]);
     modify_ghost_position(&game->ghosts[3],new_pos[3]);
+}
+
+void add_pills_to_game(Game *game,Pill new_pill){
+    game->pills = add_p_node(game->pills,new_pill);
 }
--- PaCE Man Server/Logic.h
+++ PaCE Man Server/Logic.h
@@ -5,45 +5,22 @@
 #pragma once
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
+// Pair - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 typedef struct Pair {
     int x;
     int y;
 } Pair;
 
+Pair create_pair(int x,int y);
+
+// Pac-Man - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 typedef struct Pac_Man {
     Pair pos;
     int sprite;
 } Pac_Man;
-
-typedef struct Ghost {
-    Pair pos;
-    bool active;
-    int color;
-} Ghost;
-
-typedef struct Pill {
-    Pair pos;
-} Pill;
-
-typedef struct Game {
-    unsigned int level;
-
-    unsigned int lives;
-
-    bool pill_active;
-
-    Pac_Man pac_man;
-
-    Ghost ghosts[4];
-
-    // Linked list of pills
-} Game;
-
-Pair create_pair(int x,int y);
-
-// PAC-MAN
 
 Pac_Man create_pac_man();
 
@@ -51,7 +28,12 @@ void modify_pac_man(Pac_Man* p_m_ptr,Pair new_pos,int new_sprite);
 
 void modify_pac_man_position(Pac_Man* p_m_ptr,Pair new_pos);
 
-// GHOST
+// Ghost - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+typedef struct Ghost {
+    Pair pos;
+    bool active;
+    int color;
+} Ghost;
 
 Ghost create_ghost(int color);
 
@@ -61,15 +43,44 @@ void activate_ghost(Ghost* g);
 
 void deactivate_ghost(Ghost* g);
 
-// PILL
+// Pill - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+typedef struct Pill {
+    Pair pos;
+} Pill;
 
 Pill create_pill(Pair pos);
 
-// GAME
+// Pill Linked List
+struct Linked_List_Pill {
+    Pill pill;
+    struct Linked_List_Pill *next;
+};
+
+typedef struct Linked_List_Pill *P_Node;
+
+P_Node create_p_node();
+
+P_Node add_p_node(P_Node head, Pill pill);
+
+
+// Game - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+typedef struct Game {
+    unsigned int level;
+
+    unsigned int lives;
+
+    bool pill_active;
+
+    P_Node pills;
+
+    Pac_Man pac_man;
+
+    Ghost ghosts[4];
+
+    // Linked list of pills
+} Game;
 
 Game create_game();
-
-// GAME INTERACTION
 
 void update_game_pac_man(Game *game,Pair new_pos,int new_sprite);
 
@@ -77,4 +88,4 @@ void update_game_pac_man_pos(Game *game,Pair new_pos);
 
 void update_game_ghosts_pos(Game *game,Pair* new_pos);
 
-
+void add_pills_to_game(Game *game,Pill new_pill);
